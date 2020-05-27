cf5f17bc5582338b7c60853abfe44c3c0f9e89c2
Igor Xavier <igoorlimaa@gmail.com>
2020-05-27 20:57:50 -0300

    drawable dice resources

--- app/src/main/java/com/igorx/diceroller/MainActivity.kt
+++ app/src/main/java/com/igorx/diceroller/MainActivity.kt
@@ -3,18 +3,33 @@ package com.igorx.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = dice_image
     }
 
     fun rollDice(view : View) {
         val randomInt = Random().nextInt(6) + 1
-        result_text.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
