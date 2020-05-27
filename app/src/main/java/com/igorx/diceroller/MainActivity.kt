2835158ac6ecae95a9f213ad171b25f18a0f5977
Igor Xavier <igoorlimaa@gmail.com>
2020-05-27 20:44:12 -0300

    adds random number

--- app/src/main/java/com/igorx/diceroller/MainActivity.kt
+++ app/src/main/java/com/igorx/diceroller/MainActivity.kt
@@ -2,11 +2,19 @@ package com.igorx.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun rollDice(view : View) {
+        val randomInt = Random().nextInt(6) + 1
+        result_text.text = randomInt.toString()
     }
 }
